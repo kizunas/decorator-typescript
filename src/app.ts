e1e4b5d75227c97f811ec414fc0c77449ed4c278
kizunas <8s6s7e@gmail.com>
2024-05-11 15:30:44 +0900

    デコレーターファクトリ

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
-function Logger(constructor: Function) { // デコレーターは慣例的に最初の文字を大文字にする。デコレーターは最終的には関数として扱われる。
-  console.log("ログ出力中...");
-  console.log(constructor);
+function Logger(logString: string) { // デコレーターは慣例的に最初の文字を大文字にする。デコレーターは最終的には関数として扱われる。
+  // デコレーターファクトリ
+  return function(constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  }
 }
 
-@Logger // @マークでデコレーターとして使うことを宣言する。クラスについての場合は引数は一つ。この場合はコンストラクタ関数。また、デコレーターはclassが宣言されたときに実行される。したがってPersonオブジェクトを作成中...（インスタンス化）よりも先に実行される。
+// @マークでデコレーターとして使うことを宣言する。クラスについての場合は引数は一つ。この場合はコンストラクタ関数。また、デコレーターはclassが宣言されたときに実行される。したがってPersonオブジェクトを作成中...（インスタンス化）よりも先に実行される。
+// @Logger 
+// デコレーターファクトリを使用する際は関数表記にする必要がある。つまり@Logger()とする。
+@Logger("ログ出力中 - PERSON")
 class Person {
   name = 'Max';
   
