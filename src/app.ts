ac0d6b7409acc9d4af5f8842cd6d14edd6bb32bf
kizunas <8s6s7e@gmail.com>
2024-05-11 15:47:00 +0900

    便利なデコレーター

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,23 @@ function Logger(logString: string) { // デコレーターは慣例的に最初�
   }
 }
 
+function WithTemplate(template: string, hookId: string) {
+  return function(constructor: any) {
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      // class Personのnameを表示させる
+      hookEl.querySelector('h1')!.textContent = p.name
+    }
+  }
+}
+
 // @マークでデコレーターとして使うことを宣言する。クラスについての場合は引数は一つ。この場合はコンストラクタ関数。また、デコレーターはclassが宣言されたときに実行される。したがってPersonオブジェクトを作成中...（インスタンス化）よりも先に実行される。
 // @Logger 
 // デコレーターファクトリを使用する際は関数表記にする必要がある。つまり@Logger()とする。
-@Logger("ログ出力中 - PERSON")
+// @Logger("ログ出力中 - PERSON")
+@WithTemplate("<h1>Personオブジェクト</h1>", "app")
 class Person {
   name = 'Max';
   
