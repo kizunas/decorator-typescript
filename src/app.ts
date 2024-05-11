e4fd2af01dffa8fb645029c26769825d99063a69
kizunas <8s6s7e@gmail.com>
2024-05-11 15:55:37 +0900

    複数のデコレーター（処理順序）

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,17 @@
 function Logger(logString: string) { // デコレーターは慣例的に最初の文字を大文字にする。デコレーターは最終的には関数として扱われる。
   // デコレーターファクトリ
+  console.log('Logger');
   return function(constructor: Function) {
+    console.log('Logger-デコレーター関数');
     console.log(logString);
     console.log(constructor);
   }
 }
 
 function WithTemplate(template: string, hookId: string) {
+  console.log('Template');
   return function(constructor: any) {
+    console.log('Template-デコレーター関数');
     const hookEl = document.getElementById(hookId);
     const p = new constructor();
     if (hookEl) {
@@ -21,7 +25,10 @@ function WithTemplate(template: string, hookId: string) {
 // @マークでデコレーターとして使うことを宣言する。クラスについての場合は引数は一つ。この場合はコンストラクタ関数。また、デコレーターはclassが宣言されたときに実行される。したがってPersonオブジェクトを作成中...（インスタンス化）よりも先に実行される。
 // @Logger 
 // デコレーターファクトリを使用する際は関数表記にする必要がある。つまり@Logger()とする。
-// @Logger("ログ出力中 - PERSON")
+
+// 処理は@Logger, @WithTemplateの順に実行されるが、デコレーター関数は下から上の順に実行される。
+// すなわち上の関数はLogger, Template, Template-デコレーター関数, Logger-デコレーター関数の順にコンソールへ表示されることになる。
+@Logger("ログ出力中 - PERSON")
 @WithTemplate("<h1>Personオブジェクト</h1>", "app")
 class Person {
   name = 'Max';
