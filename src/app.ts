45d71130d7bba8dfd03de8309abfdc25a9e1681d
kizunas <8s6s7e@gmail.com>
2024-05-18 16:18:49 +0900

    デコレーターによるバリデーション

--- src/app.ts
+++ src/app.ts
@@ -131,3 +131,84 @@ const p = new Printer();
 const button = document.querySelector("button")!;
 
 button.addEventListener('click', p.showMessage);
+
+
+
+interface ValidatorConfig {
+  // index型の構文を使用
+  [prop: string]: {
+    [validatableProp: string]: string[] // ['required', 'positive']のようなバリデータのリストを保存する
+  }
+}
+
+const registeredValidators: ValidatorConfig = {}; // デコレータが実行されたときに初めて値が登録される。
+
+function Required(target: any, propName: string) { // プロパティーに対するデコレータはプロパティティスクリプターを受け取らない
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]:  [
+      ...(registeredValidators[target.constructor.name]?.[propName] ?? []),
+      "required",
+    ],
+  };
+}
+
+function PositiveNumber(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]:  [
+      ...(registeredValidators[target.constructor.name]?.[propName] ?? []),
+      "positive",
+    ],
+  }
+}
+
+function validate(obj: any) {
+  const objValidatorConfig = registeredValidators[obj.constructor.name];
+  if (!objValidatorConfig) {
+    return true;
+  }
+  let isValid = true
+  for (const prop in objValidatorConfig) {
+    for (const validator of objValidatorConfig[prop]){
+      switch (validator) {
+        case 'required':
+          isValid = isValid && !!obj[prop];
+          break;
+        case 'positive':
+          isValid = isValid && obj[prop] > 0;
+          break;
+      }
+    }
+  }
+  return isValid;
+}
+
+class Course {
+  @Required
+  title: string;
+  @PositiveNumber
+  price: number;
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this.price = p;
+  }
+}
+
+const courseForm = document.querySelector('form')!;
+courseForm.addEventListener('submit', event => {
+  event.preventDefault(); // http requestを行わないため
+  const titleEl = document.getElementById('title') as HTMLInputElement;
+  const priceEl = document.getElementById('price') as HTMLInputElement;
+
+  const title = titleEl.value;
+  const price = +priceEl.value; // plusをつけてnumberにキャストする。
+
+  const createdCourse = new Course(title, price);
+  if (!validate(createdCourse)) {
+    alert('正しく入力してください！');
+    return;
+  }
+  console.log(createdCourse);
+})
